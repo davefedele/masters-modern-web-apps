cd5ca6348ad8eb69fe7fbe7520cc83462bf2e214
David Fedele <dave.fedele@gmail.com>
2016-01-05 00:20:09 -0800

    oauth with github

--- src/router.js
+++ src/router.js
@@ -1,5 +1,8 @@
+import app from 'ampersand-app'
 import React from 'react'
 import Router from 'ampersand-router'
+import qs from 'qs'
+import xhr from 'xhr'
 
 import Layout from './layout'
 import Links from './components/nav-helper'
@@ -21,7 +24,9 @@ export default Router.extend({
 
   routes: {
     '': 'public',
-    'repos': 'repos'
+    'repos': 'repos',
+    'login': 'login',
+    'auth/callback?:query': 'authCallback'
   },
 
   public () {
@@ -30,5 +35,26 @@ export default Router.extend({
 
   repos () {
     this.renderPage(<ReposPage/>)
+  },
+
+  login () {
+    window.location = 'https://github.com/login/oauth/authorize?' + qs.stringify({
+      client_id: '567338d67be7d8a760ad',
+      redirect_uri: window.location.origin + '/auth/callback',
+      scope: 'user, public_repo'
+    })
+  },
+
+  authCallback (query) {
+    query = qs.parse(query);
+    console.log(query);
+
+    xhr({
+      url: 'https://labelr--gatekeeper.herokuapp.com/authenticate/' + query.code,
+      json: true
+    }, (err, req, body) => {
+      console.log(body)
+      app.me.token = body.token
+    })
   }
 })
